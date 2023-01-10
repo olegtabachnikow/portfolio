ede2245427ae23b5cf58562ae9e3c00f3ad660bd
OlegTabachnikow <eskelll4ik@gmail.com>
2023-01-10 13:53:19 +0200

    upd cv

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "./Projects.css";
-import Skills from "../Skills/Skills";
-import Card from "../Card/Card";
-import { projectsData } from "../../constants/data";
-import gitIcon from "../../images/github-logo.svg";
+import React from 'react';
+import './Projects.css';
+import Skills from '../Skills/Skills';
+import Card from '../Card/Card';
+import { projectsData } from '../../constants/data';
+import gitIcon from '../../images/github-logo.svg';
 
 function Projects() {
   return (
-    <section id="projects" className="projects">
+    <section id='projects' className='projects'>
       <Skills />
-      <h2 className="projects__title">
+      <h2 className='projects__title'>
         And these are examples of how I implement them
       </h2>
-      <div className="projects__box">
+      <div className='projects__box'>
         {projectsData.map((item, index) => {
           return (
             <Card
@@ -26,18 +26,18 @@ function Projects() {
             />
           );
         })}
-        <div className="porjects__stuff">
-          <p className="projects__stuff-subtitle">
-            Also, some stuff still available on my Git
-          </p>
-          <a
-            href="https://github.com/Eskel4ik?tab=repositories"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img src={gitIcon} alt="github icon" />
-          </a>
-        </div>
+      </div>
+      <div className='porjects__stuff'>
+        <p className='projects__stuff-subtitle'>
+          Also, some stuff still available on my Git
+        </p>
+        <a
+          href='https://github.com/Eskel4ik?tab=repositories'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <img src={gitIcon} alt='github icon' />
+        </a>
       </div>
     </section>
   );
--- src/constants/data.js
+++ src/constants/data.js
@@ -4,61 +4,71 @@ import avaMind from '../images/ava.png';
 import homeland from '../images/homeland.png';
 import howToLearn from '../images/howtolearn.png';
 import league from '../images/league.png';
+import robo from '../images/robo.png';
 
 const projectsData = [
   {
-    name: "League of Legends DB",
-    link: "https://eskel4ik.github.io/league-of-legends-db/",
-    gitLink: "https://github.com/Eskel4ik/league-of-legends-db",
+    name: 'Get Robo VPN',
+    link: 'https://t.me/getrobovpn_bot',
+    gitLink: 'https://github.com/Eskel4ik/vpn-frontend',
     description:
-      "My pet project, dedicated to League of Legends game. You can check here info about all champions and items in game. I still working on some features.",
+      'Frontend SPA for Robo VPN services, integrated in Telegram Bot web app.',
+    preview: robo,
+    stackUsed: 'ReactJS, Javascript, HTML, CSS, DotEnv, Framer Motion',
+  },
+  {
+    name: 'League of Legends DB',
+    link: 'https://eskel4ik.github.io/league-of-legends-db/',
+    gitLink: 'https://github.com/Eskel4ik/league-of-legends-db',
+    description:
+      'My pet project, dedicated to League of Legends game. You can check here info about all champions and items in game. I still working on some features.',
     preview: league,
-    stackUsed: "ReactJS, Javascript, HTML, CSS",
+    stackUsed: 'ReactJS, Javascript, HTML, CSS',
   },
   {
-    name: "News Explorer",
-    link: "https://oleg-news-explorer.students.nomoredomainssbs.ru/",
-    gitLink: "https://github.com/Eskel4ik/news-explorer-frontend",
+    name: 'News Explorer',
+    link: 'https://oleg-news-explorer.students.nomoredomainssbs.ru/',
+    gitLink: 'https://github.com/Eskel4ik/news-explorer-frontend',
     description:
-      "This application allows you to search the latest news articles by any topic you want. Registered users can save any articles, they are interested in.",
+      'This application allows you to search the latest news articles by any topic you want. Registered users can save any articles, they are interested in.',
     preview: newsExplorer,
-    stackUsed: "HTML5, CSS, ReactJS, NodeJS, Express, MongoDB",
+    stackUsed: 'HTML5, CSS, ReactJS, NodeJS, Express, MongoDB',
   },
   {
-    name: "Around US",
-    link: "https://www.oleg.students.nomoreparties.sbs/",
-    gitLink: "https://github.com/Eskel4ik/react-around-auth",
+    name: 'Around US',
+    link: 'https://www.oleg.students.nomoreparties.sbs/',
+    gitLink: 'https://github.com/Eskel4ik/react-around-auth',
     description:
-      "Single page social network application, which allows registered users to add, like and delete photos, as well as edit their bio.",
+      'Single page social network application, which allows registered users to add, like and delete photos, as well as edit their bio.',
     preview: around,
-    stackUsed: "HTML5, CSS, ReactJS, NodeJS, Express, MongoDB",
+    stackUsed: 'HTML5, CSS, ReactJS, NodeJS, Express, MongoDB',
   },
   {
-    name: "Ava Mind",
-    link: "https://avamind.online/",
-    gitLink: "https://github.com/Eskel4ik/ava-mind-react",
+    name: 'Ava Mind',
+    link: 'https://avamind.online/',
+    gitLink: 'https://github.com/Eskel4ik/ava-mind-react',
     description:
-      "Single page Buisness card application representing a startup idea and allowing users to learn about the product. App leads users through questions about their conditions, explaining step by step why and how they can use the product.",
+      'Single page Buisness card application representing a startup idea and allowing users to learn about the product. App leads users through questions about their conditions, explaining step by step why and how they can use the product.',
     preview: avaMind,
-    stackUsed: "HTML, CSS, ReactJS",
+    stackUsed: 'HTML, CSS, ReactJS',
   },
   {
-    name: "From Homeland to Homeland",
-    link: "https://eskel4ik.github.io/homeland/index.html",
-    gitLink: "https://github.com/Eskel4ik/homeland",
+    name: 'From Homeland to Homeland',
+    link: 'https://eskel4ik.github.io/homeland/index.html',
+    gitLink: 'https://github.com/Eskel4ik/homeland',
     description:
-      "Landing page responsive template, including short articles about different places.",
+      'Landing page responsive template, including short articles about different places.',
     preview: homeland,
-    stackUsed: "HTML, CSS",
+    stackUsed: 'HTML, CSS',
   },
   {
-    name: "How to Learn",
-    link: "https://eskel4ik.github.io/how-to-learn/index.html",
-    gitLink: "https://github.com/Eskel4ik/how-to-learn",
+    name: 'How to Learn',
+    link: 'https://eskel4ik.github.io/how-to-learn/index.html',
+    gitLink: 'https://github.com/Eskel4ik/how-to-learn',
     description:
-      "A landing page which includes information about effective learning techniques and resourses.",
+      'A landing page which includes information about effective learning techniques and resourses.',
     preview: howToLearn,
-    stackUsed: "HTML, CSS",
+    stackUsed: 'HTML, CSS',
   },
 ];
 
